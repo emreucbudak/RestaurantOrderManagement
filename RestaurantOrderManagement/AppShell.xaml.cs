a85953ff059031e8693fbe70d15a049b9d0630d3
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-02 05:24:23 +0300

    Proje dosyası ekle.

---
+++ RestaurantOrderManagement/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantOrderManagement
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RestaurantOrderManagement/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace RestaurantOrderManagement
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
