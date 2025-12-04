ac9e6878991a930628bcbfb6242fd863449a0e44
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-05 00:50:32 +0300

    productcategory için gerekenler ayarlandı

---
+++ RestaurantOrderManagement/Models/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantOrderManagement.Models
+{
+    public class ProductCategory
+    {
+        public string name { get; set; }
+    }
+}
