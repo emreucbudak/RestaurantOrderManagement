f1821b1fb8e3816b2c88cc016bfdc54505ec2918
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-04 19:20:47 +0300

    masa yönetimi tamamen eklendi

---
+++ RestaurantOrderManagement/Models/AddDesk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantOrderManagement.Models
+{
+    public class AddDesk
+    {
+        public string name { get; set; }
+        public bool is_available { get; set; }
+        public int restaurant_id { get; set; }
+
+    }
+}
---
+++ RestaurantOrderManagement/Models/Desks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantOrderManagement.Models
+{
+    public class Desks
+    {
+        public string name { get; set; }
+        public bool is_available { get; set; }
+       
+    }
+}
