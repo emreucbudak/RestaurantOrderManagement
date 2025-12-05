5f6d4284af18f6e8732a151ae8beb8de94878244
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-05 21:09:39 +0300

    RESTAURANTMANAGER TARAFI TAMAMLANDI

--- RestaurantOrderManagement/Models/OrderResponse.cs
+++ RestaurantOrderManagement/Models/OrderResponse.cs
@@ -1,36 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
-namespace RestaurantOrderManagement.Models
+namespace RestaurantOrderManagement.Models;
+
+public class OrderResponse
 {
-    public class OrderResponse
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("total_price")]
+    public double TotalPrice { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("table_name")]
+    public string TableName { get; set; }
+
+    [JsonPropertyName("status_name")]
+    public string StatusName { get; set; }
+
+    [JsonPropertyName("payment_type_name")]
+    public string PaymentTypeName { get; set; }
+
+    public bool IsPreparedButtonVisible
     {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-
-        [JsonPropertyName("total_price")]
-        public double TotalPrice { get; set; }
-
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-
-        [JsonPropertyName("table_name")]
-        public string TableName { get; set; }
-
-        [JsonPropertyName("status_name")]
-        public string StatusName { get; set; }
-
-        [JsonPropertyName("payment_type_name")]
-        public string PaymentTypeName { get; set; }
-
-        public bool IsPreparedButtonVisible => StatusName == "Hazırlanıyor";
-
-        public bool IsDeliveredButtonVisible => StatusName == "Hazırlandı";
-
-        public bool IsCancelButtonVisible => StatusName != "Teslim Edildi" && StatusName != "İptal Edildi";
-
-        public bool IsCompleted => !IsCancelButtonVisible;
+        get
+        {
+            if (string.IsNullOrEmpty(StatusName)) return false;
+            return StatusName.ToLower().Contains("hazırla");
+        }
     }
+
+    public bool IsDeliveredButtonVisible
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(StatusName)) return false;
+            return StatusName.ToLower().Contains("hazırlandı");
+        }
+    }
+
+    public bool IsCancelButtonVisible
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(StatusName)) return true;
+
+            var s = StatusName.ToLower();
+            return !s.Contains("teslim") && !s.Contains("ptal");
+        }
+    }
+
+    public bool IsCompleted => !IsCancelButtonVisible;
 }
