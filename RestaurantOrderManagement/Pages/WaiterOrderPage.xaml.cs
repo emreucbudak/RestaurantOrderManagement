47200e65a71a0926e2e7cc76f8cf30f0fe225972
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-06 00:14:25 +0300

    GARSON TARAFI SAYFALAR EKLENİYOR

---
+++ RestaurantOrderManagement/Pages/WaiterOrderPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace RestaurantOrderManagement.Pages;
+
+public partial class WaiterOrderPage : ContentPage
+{
+	public WaiterOrderPage()
+	{
+		InitializeComponent();
+	}
+}
--- RestaurantOrderManagement/Pages/WaiterPanel.xaml.cs
+++ RestaurantOrderManagement/Pages/WaiterPanel.xaml.cs
@@ -6,4 +6,16 @@ public partial class WaiterPanel : ContentPage
 	{
 		InitializeComponent();
 	}
+	private async void OnLogOutClicked (object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("///MainPage");
+	}
+	private async void OnOrderAddClicked (object  sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("///WaiterOrderPage");
+	}
+	private async void OnOrderListClicked (object  sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("///WaiterOrderListPage");
+    }
 }
